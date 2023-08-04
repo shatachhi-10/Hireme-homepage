6cb891a9d4fa19b0f09c6673649e788e5e45fd77
shatachhi-10 <137662646+shatachhi-10@users.noreply.github.com>
2023-08-04 07:49:18 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        Poppins:"Poppins",
+      },
+      colors:{
+        dark_purple:"#000235",
+        btn_primary:"#7e7ee3",
+        heading_color:"#abd9d9"
+      },
+    },
+  },
+  plugins: [],
+}
+
